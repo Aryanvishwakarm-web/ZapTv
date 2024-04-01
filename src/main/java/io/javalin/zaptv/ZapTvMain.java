2cf9a5097c3d00fada0f9aa7707dfcb500c24133
AnWen <aryanvishwakarmag339@gmail.com>
2024-04-01 13:17:42 +0530

    update

--- src/main/java/io/javalin/zaptv/ZapTvMain.java
+++ src/main/java/io/javalin/zaptv/ZapTvMain.java
@@ -1,11 +1,19 @@
 package io.javalin.zaptv;
 import io.javalin.Javalin;
+import io.javalin.community.ssl.SslPlugin;
 import io.javalin.http.staticfiles.Location;
+
 
 public class ZapTvMain {
     public static void main(String[] args) {
+
+        SslPlugin plugin = new SslPlugin(conf -> {
+            conf.pemFromPath("/path/to/cert.pem", "/path/to/key.pem");
+        });
+
         Javalin.create(config -> {
             config.staticFiles.add("src/main/resources/public", Location.EXTERNAL);
+            config.registerPlugin(plugin);
             config.router.mount(router -> {
                 router.ws("/api/matchmaking", Matchmaking::websocket);
             });
